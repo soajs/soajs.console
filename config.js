c362b99079c2124cc31f56a2e1562c9caefe017a
antoinehage <me@antoinehage.com>
2020-09-16 07:59:04 -0400

    added GET - /registry/key

--- config.js
+++ config.js
@@ -66,6 +66,7 @@ module.exports = {
 		500: "Nothing to Update!",
 		501: "Unable to find the environment deployer data",
 		502: "Unable to find the environment",
+		503: "Unable to find the environment key data",
 		
 		601: "Model not found",
 		602: "Model error: ",
@@ -179,6 +180,13 @@ module.exports = {
 			"/registry": {
 				"_apiInfo": {
 					"l": "This API gets a registry",
+					"group": "Registry"
+				},
+				"commonFields": ["env"]
+			},
+			"/registry/key": {
+				"_apiInfo": {
+					"l": "This API gets a registry key",
 					"group": "Registry"
 				},
 				"commonFields": ["env"]
